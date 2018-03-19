0b76ffe6696e1fb954e6340e30f6155d59ae4f50
i_jemin <i_jemin@hotmail.com>
2018-03-20 06:54:07 +0900

    Module is not global

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-console.log("Hello World!");
-
-setTimeout();
-clearTimeout();
-
-setInterval(); // repeat call after delay
-clearInterval();
-
-//window.console.log
-
-var message = '';
-console.log(global.message); // won't work
-//global.setTimeout
+console.log(module); // module is not global
