088989a4593b6cd70d33d883972b5b6ad0747a4f
I_Jemin <i_jemin@hotmail.com>
2018-03-20 06:46:08 +0900

    Node Global

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+setTimeout();
+clearTimeout();
+
+setInterval(); // repeat call after delay
+clearInterval();
+
+//window.console.log
+
+var message = '';
+console.log(global.message); // won't work
+//global.setTimeout
