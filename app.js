69e2f3470767cdd7c3ed9644ad615c38d95790c2
i_jemin <i_jemin@hotmail.com>
2018-03-21 23:49:18 +0900

    Event Argument

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const log = require('./logger')
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on('messageLogged', function(arg) { // e, eventArg
+    console.log('Listener called',arg);
+});
+
+emitter.emit('messageLogged', {id: 1, url: 'http://' });
 
 
-log('message');
