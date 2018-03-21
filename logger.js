87958a8b97c6ee66d8334749f92949b5b9c3f7ab
i_jemin <i_jemin@hotmail.com>
2018-03-22 00:21:31 +0900

    Extends Class

--- logger.js
+++ logger.js
@@ -1,15 +1,15 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 var url = 'http:mylogger.io/log';
 
-function log(message) {
-    // Send an HTTP request
-    console.log(message);
-    emitter.emit('messageLogged', {id: 1, url: 'http://' });
+// function in class won't need function keyword
+class Logger extends EventEmitter {
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+
+        //Raise an event
+        this.emit('messageLogged', {id: 1, url: 'http://' });
+    }
 }
 
-
-
-
-
-module.exports = log;
+module.exports = Logger; 
