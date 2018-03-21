c06cc8be50e25cc58f70ad4f4c1b018f64c11023
i_jemin <i_jemin@hotmail.com>
2018-03-22 00:05:03 +0900

    Emitter Practice

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ emitter.on('messageLogged', (arg) => {
     console.log('Listener called',arg);
 });
 
+emitter.on('logging', (e) => { console.log('Loggin Event',e);});
+
 emitter.emit('messageLogged', {id: 1, url: 'http://' });
 
-
+// Raise: logging (data: message)
+emitter.emit('logging','Jemin');
