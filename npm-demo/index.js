e3b31f6fd5d1af5bc2437f093b201c16e8a22b4c
i_jemin <i_jemin@hotmail.com>
2018-03-22 18:49:48 +0900

    Use Package

---
+++ npm-demo/index.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+// Finding path
+// First: Core Module
+// Secondary: File or Folder
+// Third: Local Packages (node_modules);
+
+
+var result = _.contains([1,2,3,],2);
+console.log(result);
