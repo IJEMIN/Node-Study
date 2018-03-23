dd741afee546de7d613f630574d44451d67ae35e
i_jemin <i_jemin@hotmail.com>
2018-03-24 00:31:49 +0900

    Handling GET Request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+	{ id: 1, name: 'course1'},
+	{ id: 2, name: 'course2'},
+	{ id: 3, name: 'course3'},
+];
+
 // Corespond to HTTP
 // app.get()
 // app.post()
@@ -12,16 +18,15 @@ app.get('/',(req,res)=> {
 });
 
 app.get('/api/courses',(req,res) => {
-	res.send([1,2,3]);
+	res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res) => {
-	res.send(req.params.id);
+	const course = courses.find(c=> c.id === parseInt(req.params.id));
+	if(!course) res.status(404).send('The course with the given ID was not found');
+	res.send(course);
 });
 
-app.get('/api/courses/:year/:month',(req,res) => {
-	res.send(req.query);
-});
 
 // PORT
 const port = process.env.PORT || 3000;
