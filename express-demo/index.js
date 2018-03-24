6993f67f9747db6005a1d3ffce8535dd49cf546e
i_jemin <i_jemin@hotmail.com>
2018-03-25 01:04:26 +0900

    Validate Input

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,13 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 const courses = [
-	{ id: 1, name: 'course1'},
-	{ id: 2, name: 'course2'},
-	{ id: 3, name: 'course3'},
+	{ id: 1, name: 'course1' },
+	{ id: 2, name: 'course2' },
+	{ id: 3, name: 'course3' },
 ];
 
 // Corespond to HTTP
@@ -15,21 +16,34 @@ const courses = [
 // app.put();
 // app.delete()
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
 	res.send('Hello World!!!');
 });
 
-app.get('/api/courses',(req,res) => {
+app.get('/api/courses', (req, res) => {
 	res.send(courses);
 });
 
-app.get('/api/courses/:id',(req,res) => {
-	const course = courses.find(c=> c.id === parseInt(req.params.id));
-	if(!course) res.status(404).send('The course with the given ID was not found');
+app.get('/api/courses/:id', (req, res) => {
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if (!course) res.status(404).send('The course with the given ID was not found');
 	res.send(course);
 });
 
-app.post('/api/courses',(req,res)=> {
+app.post('/api/courses', (req, res) => {
+	// Define Schema
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) {
+		// 400 Bad Request
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name
@@ -42,4 +56,4 @@ app.post('/api/courses',(req,res)=> {
 // PORT
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=> console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
