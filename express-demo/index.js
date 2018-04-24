1ba411ecfc791ee30f416963ae4ce43909609fff
i_jemin <i_jemin@hotmail.com>
2018-04-25 00:46:34 +0900

    Built in middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-//app.use(express.urlencoded()); // key=value&key=value
+app.use(express.urlencoded({extended: true})); // key=value&key=value
+app.use(express.static('public'));
 
 app.use(logger);
 
