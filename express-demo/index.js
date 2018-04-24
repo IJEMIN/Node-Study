31e5511221b2309d90593a70139ecfe34a39a8ea
i_jemin <i_jemin@hotmail.com>
2018-04-25 00:42:33 +0900

    Custom Middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,18 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+//app.use(express.urlencoded()); // key=value&key=value
+
+app.use(logger);
+
+app.use(function(req,res,next) {
+	console.log('Authenticating...');
+	next(); //without this, control terminated
+});
+
 
 const courses = [
 	{ id: 1, name: 'course1' },
