0f05a9556de413a8b411a07e2f1d7a67861a12b6
i_jemin <i_jemin@hotmail.com>
2018-05-03 21:06:56 +0900

    Async and Await

--- async-demo/index.js
+++ async-demo/index.js
@@ -11,12 +11,24 @@ console.log('Before');
 // });
 
 // Promise-based approach
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log('Commits', commits))
-  .catch(err => console.log('Error', err.message));
 
+
+// Asyncs and Await approach
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch (err) {
+    console.log('Error', err.message);
+
+  }
+}
+
+displayCommits();
 
 console.log('After');
 
@@ -35,7 +47,7 @@ function getRepositories(username) {
     setTimeout(() => {
       console.log('Calling GitHub API...');
       resolve(['repo1', 'repo2', 'repo3']);
-    }, 2000);  
+    }, 2000);
   });
 }
 
