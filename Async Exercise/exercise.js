a628999f8cd11a7c0012129b3f20f48c1eb7c7a3
i_jemin <i_jemin@hotmail.com>
2018-05-04 21:25:30 +0900

    Done Exercise

--- Async Exercise/exercise.js
+++ Async Exercise/exercise.js
@@ -1,35 +1,56 @@
 
-getCustomer(1, (customer) => {
+
+async function notifyCustomer(id) {
+
+  const customer = await getCustomer(id);
+
   console.log('Customer: ', customer);
+
   if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
+    const movies = await getTopMovies();
+
+    console.log('Top movies: ', movies);
+
+    await sendEmail(customer.email,movies);
+
+    console.log('Email sent...');
+
   }
-});
 
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+notifyCustomer(1);
+
+
+function getCustomer(id) {
+
+  return new Promise(
+    (resolve) => {
+      setTimeout(() => {
+        resolve({
+          id: 1,
+          name: 'Mosh Hamedani',
+          isGold: true,
+          email: 'email'
+        });
+      }, 4000);
+    }
+
+  );
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function getTopMovies() {
+
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000))
+}
+
+function sendEmail(email, movies) {
+
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 4000))
 }
