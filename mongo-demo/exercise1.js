08e16366b74450b36e3362176268691d64151f82
i_jemin <i_jemin@hotmail.com>
2018-05-07 23:38:28 +0900

    Fix Exercise 1

--- mongo-demo/exercise1.js
+++ mongo-demo/exercise1.js
@@ -8,25 +8,29 @@ mongoose.connect('mongodb://localhost/mongo-exercises')
 
 
 const courseSchema = new mongoose.Schema({
-    tags: [String],
-    data: { type: Date, default: Date.now},
     name: String,
     author: String,
+    tags: [String],
+    data: Date,
     isPublished: Boolean,
     price: Number
 });
 
-const Course = mongoose.model('Course',courseSchema);
+const Course = mongoose.model('Course', courseSchema);
 
 async function getCourses() {
 
-    const courses = await Course
-        .find({isPublished: true, tags: 'backend'})
-        .sort({name: 1})
-        .select({name: 1, author: 1});
-
-    console.log(courses);
-
+    return await Course
+        .find({ isPublished: true, tags: 'backend' })
+        .sort({ name: 1 })
+        .select({ name: 1, author: 1 });
 }
 
-getCourses();
+async function displayCourses() {
+
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+
+displayCourses();
