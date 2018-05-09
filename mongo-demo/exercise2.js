06510a41631affa464f571a149ebf4f8ffeb2bb2
i_jemin <i_jemin@hotmail.com>
2018-05-09 13:34:21 +0900

    Apply Solution2

--- mongo-demo/exercise2.js
+++ mongo-demo/exercise2.js
@@ -1,37 +1,29 @@
-
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongo-exercises')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.log('Could not connect to MongoDB...', err));
-
-
+mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    data: Date,
-    isPublished: Boolean,
-    price: Number
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function getCourses() {
-
-    return await Course
-        .find({ isPublished: true})
-        .or([{tags: 'frontend'}, {tags: 'backend'}])
-        .sort({ price: -1 })
-        .select({ name: 1, author: 1,price: 1 });
+  return await Course
+  .find({ isPublished: true })
+  .or([ { tags: 'frontend' }, { tags: 'backend' } ])
+  .sort('-price')
+  .select('name author price');
 }
 
-async function displayCourses() {
-
-    const courses = await getCourses();
-    console.log(courses);
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
 }
 
-
-displayCourses();
+run();
