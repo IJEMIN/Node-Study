e6ef227f2f80d541d48cac5b2b8b116d17067392
i_jemin <i_jemin@hotmail.com>
2018-05-19 14:32:29 +0900

    Custom Validator

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -19,7 +19,15 @@ const courseSchema = new mongoose.Schema({
         enum: ['web', 'mobile', 'network']
     },
     author: String,
-    tags: [String],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least on tag'
+        }
+    },
     data: { type: Date, default: Date.now },
     isPublished: Boolean,
     price: {
@@ -36,9 +44,8 @@ async function createCourse() {
 
     const course = new Course({
         name: 'Angular Course',
-        category: '-',
+        category: 'web',
         author: 'Mosh',
-        tags: ['angular', 'frontend'],
         isPublished: true,
         price: 15
     });
