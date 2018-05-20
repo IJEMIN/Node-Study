11b219f36fdec479f843b79c8edc4b2220e59b2b
i_jemin <i_jemin@hotmail.com>
2018-05-20 16:50:49 +0900

    Project- Add Persistent to Genres API

--- vidly/index.js
+++ vidly/index.js
@@ -1,14 +1,14 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
-const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json()); // Use Middleware
-app.use('/api/genres/',genres);
+app.use('/api/genres', genres);
 
-
-app.get('/', (req, res) => res.send('Welcome to Vidly'));
-
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
